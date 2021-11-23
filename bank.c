b8e7e84dd7a5f2d39decf44c0164339bd19515e2
nika328 <63118962+nika328@users.noreply.github.com>
2021-11-23 13:02:49 +0100

    Update bank.c

--- bank.c
+++ bank.c
@@ -5,78 +5,7 @@ int main()
 {
     int num;
     printf("Enter the value you want to withdraw from the bank: ");
-    scanf("%d ", &num);
-
-   
-        if(num/500>0)
-        {
-            printf("You will be given %d 500 dollar bill", num/500);
-            num=num%500;
-        }
-        if(num/200>0)
-        {
-            printf("You will be given %d 200 dollar bill", num/200);
-            num=num%200;
-        }
-        if(num/100>0)
-        {
-            printf("You will be given %d 100 dollar bill", num/100);
-            num=num%100;
-        }
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    int num;
-    printf("Enter the value you want to withdraw from the bank: ");
     scanf("%d", &num);
-
-   /*
-        if((num/500)>0)
-        {
-            printf("You will be given %d 500 dollar bill\n", num/500);
-            num=num%500;
-        }
-        if((num/200)>0)
-        {
-            printf("You will be given %d 200 dollar bill\n", num/200);
-            num=num%200;
-        }
-        if((num/100)>0)
-        {
-            printf("You will be given %d 100 dollar bill\n", num/100);
-            num=num%100;
-        }
-        if((num/50)>0)
-        {
-            printf("You will be given %d 50 dollar bill\n", num/50);
-            num=num%50;
-        }
-        if((num/20)>0)
-        {
-            printf("You will be given %d 20 dollar bill\n", num/20);
-            num=num%20;
-        }
-        if((num/10)>0)
-        {
-            printf("You will be given %d 10 dollar bill\n", num/10);
-            num=num%10;
-        }
-        if((num/5)>0)
-        {
-            printf("You will be given %d 5 dollar bill\n", num/5);
-            num=num%5;
-        }
-        if((num/2)>0)
-        {
-            printf("You will be given %d 2 dollar bill\n", num/2);
-            num=num%2;
-        }
-        if(num/1>0)
-        {
-            printf("You will be given %d 1 dollar bill\n\n\n", num/1);
-        }*/
 
         int arr[]={1,2,5,10,20,50,100,200,500};
         int i=1;
